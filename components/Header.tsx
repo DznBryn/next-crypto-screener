633b0ca09ee6cc72d3e9c9efbc8938b255a8c5bb
dznbryn <bryan@debybe.com>
2026-01-06 11:25:59 -0500

    added navigation

---
+++ components/Header.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { cn } from '@/lib/utils';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+function Header() {
+	const pathname = usePathname();
+	return (
+		<header>
+			<div className='main-container inner'>
+				<Link href='/'>
+					<Image src='logo.svg' alt='CoinPulse Logo' width={150} height={50} />
+				</Link>
+				<nav>
+					<Link
+						href='/'
+						className={cn('nav-link', {
+							'is-active': pathname === '/',
+							'is-home': true,
+						})}>
+						Home
+					</Link>
+					<p>Search Modal</p>
+					<Link
+						href='/coins'
+						className={cn('nav-link', {
+							'is-active': pathname === '/coins',
+						})}>
+						All coins
+					</Link>
+				</nav>
+			</div>
+		</header>
+	);
+}
+
+export default Header;
